3e63d13f32f22dfcdf15f4693455caf398d4c238
robertdegijt@live.nl <robertdegijt@live.nl>
2021-02-02 19:06:10 +0100

    Added different alert message

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDom from 'react-dom';
 
 import './index.css';
 
+const alertMessage = () => {
+  alert('BOOOOOOOOOEEEEEM!!!!');
+};
+
 const Greeting = () => {
   return (
     <div className='greeting'>
       <h1>Hello World!</h1>
       <p>hello</p>
+      <button onClick={alertMessage}>DON'T CLICK</button>
     </div>
   );
 };
